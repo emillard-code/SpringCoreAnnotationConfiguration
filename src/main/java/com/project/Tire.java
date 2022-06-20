fa709dbea8bd5da9fb394ff975ac01781cfe1a61
Emerson Millard <emillard.nt@gmail.com>
2022-06-19 21:28:21 -0700

    Added additional annotations.

--- src/main/java/com/project/Tire.java
+++ src/main/java/com/project/Tire.java
@@ -1,10 +1,12 @@
 package com.project;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Tire {
 
+    @Value("Firestone")
     private String brand;
 
     public String getBrand() {
